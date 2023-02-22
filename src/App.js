571d406bf51a1c8ce787ea4d4fc7bc56908973a5
benderang26 <benderang26@gmail.com>
2023-02-22 19:41:46 +0800

    add router, material ui dependencies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react'
 
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+
 function App() {
   return (
-    <div>
-      <h1>Ang Online Shopping</h1>
-    </div>
+    <BrowserRouter>
+        <Routes>
+          <Route path='/register' element={RegisterPage}/>
+          <Route path='/login' element={LoginPage}/>
+          <Route path='/' element={HomePage}/>
+        </Routes>
+    </BrowserRouter>
   );
 }
 
